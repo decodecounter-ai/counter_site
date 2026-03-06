ddf079de2df9624966ab5b30934b9af406faa92b
decodecounter-ai <decodero2counter@gmail.com>
2026-03-06 12:23:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-// Paste your Google Apps Script URL here only once:
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwMB052wRnuglvJm3_04OyVg7rVWY4blVC9Wlvp3wpE-4cg9P9ZRVjFqp7YZf4bokDVFg/exec";
